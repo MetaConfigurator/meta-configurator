6e487365fc879ff68074edff8901a5c8340a36ae
Keyshal-211 <kkpatel.de@gmail.com>
2023-05-30 15:47:17 +0200

    delete some code

--- public/MongoDB/database.py
+++
@@ -1,5 +0,0 @@
-def GetconnectionString():
-    connectionString="mongodb+srv://<username>:<password>@cluster0.oclxqaq.mongodb.net/"
-    return connectionString
-
-#you should write down your username and password instaed of <username> and <password>
