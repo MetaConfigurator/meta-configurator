baa4db84c3562b9aa6aaadaa5dda8e7fdca8b746
Keyshal-211 <kkpatel.de@gmail.com>
2023-06-21 15:33:04 +0200

    Add a main file

--- frontend/meta-configurator/src/main.ts
+++ frontend/meta-configurator/src/main.ts
@@ -6,7 +6,7 @@ import App from '@/App.vue';
 import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
+
 
 const app = createApp(App);
 
