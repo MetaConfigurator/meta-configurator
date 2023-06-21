8c0703038670e39eb3b6b9deedeca0e9c454c121
Keyshal-211 <kkpatel.de@gmail.com>
2023-06-21 16:09:20 +0200

    Adding missing files

--- frontend/meta-configurator/src/main.ts
+++ frontend/meta-configurator/src/main.ts
@@ -4,10 +4,9 @@ import router from './router';
 // @ts-ignore
 import App from '@/App.vue';
 import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
-
-
+import 'primevue/resources/themes/lara-light-indigo/theme.css';
+import 'primeicons/primeicons.css';
 const app = createApp(App);
 
 app.use(createPinia());
