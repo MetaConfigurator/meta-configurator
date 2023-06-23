28cd4cd45f8bf8d6d5d37ac887658d8ecdf9b557
Keyshal-211 <kkpatel.de@gmail.com>
2023-06-23 13:38:14 +0200

    Add DropDownMenu

--- frontend/meta-configurator/src/App.vue
+++ frontend/meta-configurator/src/App.vue
@@ -5,6 +5,8 @@ import {SideMenuController} from '@/components/side-menu/SideMenu';
 import GuiEditorPanel from '@/components/gui-editor/JsonSchemaGuiEditorPanel.vue';
 import 'primeicons/primeicons.css';
 import AceEditor from '@/components/code-editor/AceEditor.vue';
+import IconDropDown from "@/components/icons/IconDropDown.vue";
+
 
 const currentFile = ref('config.yaml');
 
@@ -25,10 +27,12 @@ const currentTitle = computed(
       <div
         class="w-full h-16 bg-slate-100 flex flex-row items-center p-2 px-6 border-b-2 border-gray-600 space-x-6">
         <h2 class="text-3xl text-gray-700" v-html="currentTitle"></h2>
+        <IconDropDown></IconDropDown>
       </div>
       <div class="flex flex-row">
         <AceEditor class="flex-initial w-full" />
         <GuiEditorPanel class="w-full" />
+
       </div>
     </main>
   </div>
---
+++ frontend/meta-configurator/src/components/icons/IconDropDown.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <Dropdown v-model="selectedOption" :options="options" optionLabel="label" placeholder="Select an language" class="w-full md:w-14rem">
+      <template #option="optionProps">
+        <li @click="handleOptionClick(optionProps.option)">
+          {{ optionProps.option.label }}
+        </li>
+      </template>
+    </Dropdown>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent} from 'vue';
+import Dropdown from 'primevue/dropdown';
+import { PrimeIcons } from 'primevue/api';
+
+interface Option {
+  label: string;
+  value: string;
+  icon?: string;
+}
+
+export default defineComponent({
+  components: {
+    Dropdown,
+  },
+  data() {
+    return {
+      selectedOption: null as Option | null,
+      options: [
+        { label: 'JSON', value: 'option1', icon: PrimeIcons.CHECK },
+        { label: 'YAML', value: 'option2', icon: PrimeIcons.CHECK },
+
+      ] as Option[],
+    };
+  },
+  methods: {
+    handleOptionClick(option: Option) {
+      // Handle the option click event here
+      console.log('Clicked option:', option);
+    },
+  },
+});
+</script>
+
+<style>
+/* Add any custom styles here */
+</style>
