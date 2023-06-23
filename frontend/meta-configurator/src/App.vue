8bf2344dc7f2f2417eabc5142d75c835bb3b29cf
Keyuri_Patel <129445010+Keyshal-211@users.noreply.github.com>
2023-06-23 14:36:22 +0200

    84 resizable panels (#85)
    * Add Splitter.
    
    * Delete Resizepanel file adding in main file
    
    ---------
    
    Co-authored-by: Keyshal-211 <Keyshal-211@users.noreply.github.com>

--- frontend/meta-configurator/src/App.vue
+++ frontend/meta-configurator/src/App.vue
@@ -2,9 +2,12 @@
 import {computed, reactive, ref} from 'vue';
 import SideMenu from '@/components/side-menu/SideMenu.vue';
 import {SideMenuController} from '@/components/side-menu/SideMenu';
-import GuiEditorPanel from '@/components/gui-editor/JsonSchemaGuiEditorPanel.vue';
 import 'primeicons/primeicons.css';
+
+import SplitterPanel from 'primevue/splitterpanel';
 import AceEditor from '@/components/code-editor/AceEditor.vue';
+import GuiEditorPanel from '@/components/gui-editor/JsonSchemaGuiEditorPanel.vue';
+import Splitter from 'primevue/splitter';
 
 const currentFile = ref('config.yaml');
 
@@ -26,9 +29,18 @@ const currentTitle = computed(
         class="w-full h-16 bg-slate-100 flex flex-row items-center p-2 px-6 border-b-2 border-gray-600 space-x-6">
         <h2 class="text-3xl text-gray-700" v-html="currentTitle"></h2>
       </div>
-      <div class="flex flex-row">
-        <AceEditor class="flex-initial w-full" />
-        <GuiEditorPanel class="w-full" />
+      <div>
+        <Splitter>
+          <!-- Left panel for the GUI editor -->
+          <SplitterPanel>
+            <AceEditor />
+          </SplitterPanel>
+
+          <!-- Right panel for the text editor -->
+          <SplitterPanel class="flex align-items-center justify-content-center" :size="80">
+            <GuiEditorPanel />
+          </SplitterPanel>
+        </Splitter>
       </div>
     </main>
   </div>
