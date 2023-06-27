564f7c3444e2ba51db98663970c08344763b9b85
minye <st170095@stud.uni-stuttgart.de>
2023-06-27 14:59:44 +0200

    move back to right side to keep it consistent visually

--- frontend/meta-configurator/src/components/gui-editor/SchemaInfoPanel.vue
+++ frontend/meta-configurator/src/components/gui-editor/SchemaInfoPanel.vue
@@ -8,8 +8,6 @@ const props = defineProps<{
   schema: ref<TopLevelJsonSchema>;
 }>();
 
-// const collapsed = ref(false);
-
 const schemaInformation = computed(() => {
   return [
     {
@@ -29,24 +27,6 @@ const schemaInformation = computed(() => {
 </script>
 
 <template>
-  <!--  <div class="p-2 bg-slate-100 rounded-xl w-full drop-shadow" :class="{'max-h-16': collapsed}">-->
-  <!--    <div class="flex flex-row items-center mb-1 cursor-pointer" @click="collapsed = !collapsed">-->
-  <!--      <ChevronRight-->
-  <!--          class="hover:scale-105 transition-all duration-200"-->
-  <!--          :class="{'transform rotate-90': !collapsed}" />-->
-  <!--      <h1 class="text-xl ml-1 font-semibold hover:underline underline-offset-4">-->
-  <!--        Schema information-->
-  <!--      </h1>-->
-  <!--    </div>-->
-
-  <!--    <div v-show="!collapsed">-->
-  <!--      <p v-for="info in schemaInformation" :key="info.title">-->
-  <!--        <span class="font-semibold">{{ info.title }}: </span>-->
-  <!--        {{ info.value }}-->
-  <!--      </p>-->
-  <!--    </div>-->
-  <!--  </div>-->
-
   <Accordion :activeIndex="1">
     <AccordionTab header="Schema Information">
       <p v-for="info in schemaInformation" :key="info.title">
