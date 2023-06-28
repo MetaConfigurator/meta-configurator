6abae95bee7f90d9d3a8cbcc1fc47ccf5a63c26a
minye <st170095@stud.uni-stuttgart.de>
2023-06-28 22:47:42 +0200

    resolve the build error caused by YAML editor

--- frontend/meta-configurator/src/components/code-editor/AceEditorForYaml.vue
+++ frontend/meta-configurator/src/components/code-editor/AceEditorForYaml.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {onMounted, ref, watch} from 'vue';
-import {dataStore} from '@/store/dataStore';
 import {storeToRefs} from 'pinia';
+import _ from 'lodash';
 import * as ace from 'brace';
 import 'brace/mode/javascript';
 import 'brace/mode/yaml';
@@ -9,20 +9,25 @@ import 'brace/theme/clouds';
 import 'brace/theme/ambiance';
 import 'brace/theme/monokai';
 import YAML from 'yaml';
+import {useDataStore} from '@/store/dataStore';
 
-const store = dataStore();
-const {configData, currentPath} = storeToRefs(store);
+const store = useDataStore();
+import type {Path} from '@/model/path';
+import {useCommonStore} from '@/store/commonStore';
+
+const {currentPath} = storeToRefs(useCommonStore());
+const {configData} = storeToRefs(useDataStore());
 const editor = ref();
 
 onMounted(() => {
-  // Set up editor mode to JSON and define theme
+  // Set up editor mode to YAML and define theme
   editor.value = ace.edit('javascript-editor');
   editor.value.getSession().setMode('ace/mode/yaml');
   editor.value.setTheme('ace/theme/clouds');
   editor.value.setShowPrintMargin(false);
 
   // Feed config data from store into editor
-  updateEditorValue(store.configData, store.currentPath);
+  updateEditorValue(store.configData, currentPath.value);
 
   // Listen to changes on AceEditor and update store accordingly
   editor.value.on('change', () => {
@@ -38,7 +43,7 @@ onMounted(() => {
     configData,
     newVal => {
       if (editor.value) {
-        updateEditorValue(newVal, store.currentPath);
+        updateEditorValue(newVal, currentPath.value);
       }
     },
     {deep: true}
@@ -48,29 +53,30 @@ onMounted(() => {
     currentPath,
     newVal => {
       if (editor.value) {
-        updateSelectedPath(newVal, store.currentPath);
+        updateSelectedPath(newVal, currentPath.value);
       }
     },
     {deep: true}
   );
 });
 
-function updateEditorValue(configData, currentPath: (string | number)[]) {
-  const currEditorContent = editor.value.getValue();
-  const newEditorContent = YAML.stringify(configData, null, 2);
-  if (currEditorContent !== newEditorContent) {
+function updateEditorValue(configData, currentPath: Path) {
+  const currEditorConfigObject =
+      editor.value.getValue() != '' ? YAML.parse(editor.value.getValue()) : {};
+  if (!_.isEqual(currEditorConfigObject, configData)) {
     // Update value with new data and also update cursor position
+    const newEditorContent = YAML.stringify(configData, null, 2);
     editor.value.setValue(newEditorContent);
     updateSelectedPath(configData, currentPath);
   }
 }
 
-function updateSelectedPath(configData, currentPath: (string | number)[]) {
+function updateSelectedPath(configData, currentPath: Path) {
   let line = determineCursorLine(configData, currentPath);
   editor.value.gotoLine(line);
 }
 
-function determineCursorLine(configData, currentPath: (string | number)[]): number {
+function determineCursorLine(configData, currentPath: Path): number {
   // todo: implement
   return 3;
 }
