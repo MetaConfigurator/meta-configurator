72366d1c0b5bcec9c28065c0573fc06529c6d713
Logende <Logende@users.noreply.github.com>
2023-07-05 19:17:20 +0000

    apply formatting changes

--- frontend/meta-configurator/src/components/gui-editor/PropertyMetadata.vue
+++ frontend/meta-configurator/src/components/gui-editor/PropertyMetadata.vue
@@ -18,11 +18,11 @@ function isExpandable(): boolean {
 }
 
 function isRequired(): boolean {
-    return props.nodeData.parentSchema?.isRequired(props.nodeData.name as string) || false;
+  return props.nodeData.parentSchema?.isRequired(props.nodeData.name as string) || false;
 }
 
 function isDeprecated(): boolean {
-    return props.nodeData.schema.deprecated;
+  return props.nodeData.schema.deprecated;
 }
 
 function zoomIntoPath() {
@@ -34,14 +34,17 @@ function zoomIntoPath() {
 
 <template>
   <!--If expandable: show underline on hovering. Call zoom function when double click. -->
-  <span class="mr-2" :class="{'hover:underline': isExpandable()}" @dblclick="zoomIntoPath()"
-  v-tooltip="nodeData.schema.description">
-      <!--If deprecated: put name into a s tag (strikethrough) -->
-      <s v-if="isDeprecated()">{{ nodeData.name }}</s>
-      <!--Otherwise: just normal text -->
-      <span v-else>{{ nodeData.name }}</span>
-      <!--Show red star after text if property is required -->
-      <span class="text-red-600">{{ isRequired() ? '*' : '' }}</span>
+  <span
+    class="mr-2"
+    :class="{'hover:underline': isExpandable()}"
+    @dblclick="zoomIntoPath()"
+    v-tooltip="nodeData.schema.description">
+    <!--If deprecated: put name into a s tag (strikethrough) -->
+    <s v-if="isDeprecated()">{{ nodeData.name }}</s>
+    <!--Otherwise: just normal text -->
+    <span v-else>{{ nodeData.name }}</span>
+    <!--Show red star after text if property is required -->
+    <span class="text-red-600">{{ isRequired() ? '*' : '' }}</span>
   </span>
 
   <span class="text-xs text-gray-400">:&nbsp;{{ nodeData.schema.type.join(',') }}</span>
