a03160ff8e0f4ca91ef7ebb0c63326561ea8f305
Keyshal-211 <kkpatel.de@gmail.com>
2023-07-07 11:16:50 +0200

    null to blank json object

--- frontend/meta-configurator/src/components/toolbar/ClearContent.ts
+++ frontend/meta-configurator/src/components/toolbar/ClearContent.ts
@@ -1,7 +1,7 @@
 import {useDataStore} from '@/store/dataStore';
 export function clearEditor(): void {
   console.log('clearEditor called');
-  useDataStore().fileData = null;
+  useDataStore().fileData = {};
 
   console.log('Cleared fileData:', useDataStore().fileData);
 }
