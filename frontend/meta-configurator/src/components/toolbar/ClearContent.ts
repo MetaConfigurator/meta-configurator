f528c5f7633c7e03e5d4416749fc112fa504b1a0
Keyshal-211 <kkpatel.de@gmail.com>
2023-07-07 11:22:24 +0200

    adding warning before deleting

--- frontend/meta-configurator/src/components/toolbar/ClearContent.ts
+++ frontend/meta-configurator/src/components/toolbar/ClearContent.ts
@@ -1,7 +1,16 @@
-import {useDataStore} from '@/store/dataStore';
+import { useDataStore } from '@/store/dataStore';
+
 export function clearEditor(): void {
   console.log('clearEditor called');
-  useDataStore().fileData = {};
 
-  console.log('Cleared fileData:', useDataStore().fileData);
+  // Show confirmation dialog
+  const confirmClear = window.confirm('Are you sure you want to clear the editor?');
+
+  if (confirmClear) {
+    // User confirmed, clear the editor
+    useDataStore().fileData = {};
+
+    console.log('Cleared fileData:', useDataStore().fileData);
+  }
 }
+
