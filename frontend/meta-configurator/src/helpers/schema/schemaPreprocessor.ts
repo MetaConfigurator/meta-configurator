c6de3a8e2f9962dd4e69cb4969cc18c87dde8bf2
PaulBredl <paulbredl@gmx.de>
2023-07-24 17:40:38 +0200

    make sure that the editor schema is not modified by preprocessing

--- frontend/meta-configurator/src/helpers/schema/schemaPreprocessor.ts
+++ frontend/meta-configurator/src/helpers/schema/schemaPreprocessor.ts
@@ -12,33 +12,36 @@ import {nonBooleanSchema} from '@/helpers/schema/SchemaUtils';
  * @returns the preprocessed schema
  */
 export function preprocessSchema(schema: JsonSchemaObjectType): JsonSchemaObjectType {
+  let copiedSchema = {...schema}; // shallow copy to prevent changing the original schema
   // TODO: resolve refs once at the beginning using json-schema-ref-parser
   // this is technically possible because the json schema faker also uses json-schema-ref-parser
   // and it works there
   // so we have to find a way to make it work here as well
-  if (hasRef(schema)) {
+  if (hasRef(copiedSchema)) {
     // remove leading # from ref if present
-    const refString = schema.$ref?.startsWith('#') ? schema.$ref.substring(1) : schema.$ref!!;
+    const refString = copiedSchema.$ref?.startsWith('#')
+      ? copiedSchema.$ref.substring(1)
+      : copiedSchema.$ref!!;
     let refSchema = pointer.get(
       nonBooleanSchema(useSessionStore().fileSchemaData ?? {}) ?? {},
       refString
     );
     refSchema = preprocessSchema(refSchema);
-    delete schema.$ref;
-    schema = {allOf: [schema, refSchema]};
+    delete copiedSchema.$ref;
+    copiedSchema = {allOf: [copiedSchema, refSchema]};
   }
 
-  if (hasAllOfs(schema)) {
+  if (hasAllOfs(copiedSchema)) {
     // @ts-ignore
-    schema.allOf = schema.allOf!!.map(preprocessSchema);
-    schema = mergeAllOf(schema, {
+    copiedSchema.allOf = copiedSchema.allOf!!.map(preprocessSchema);
+    copiedSchema = mergeAllOf(copiedSchema, {
       resolvers: {
         defaultResolver: mergeAllOf.options.resolvers.title,
       },
     });
   }
 
-  return schema;
+  return copiedSchema;
 }
 
 function hasRef(schema: JsonSchemaObjectType): boolean {
