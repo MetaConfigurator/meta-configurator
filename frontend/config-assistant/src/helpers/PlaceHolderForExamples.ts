bea5b17eaa5fcb5eb1a2ecc48e59b5847f28d1cd
beyond2void <minyexu@gmail.com>
2023-08-01 11:52:32 +0200

    improve

--- frontend/config-assistant/src/helpers/PlaceHolderForExamples.ts
+++ frontend/config-assistant/src/helpers/PlaceHolderForExamples.ts
@@ -1,7 +1,11 @@
 import {JsonSchema} from '@/helpers/schema/JsonSchema';
 
 export function placeHolderValue(schema: JsonSchema) {
-  return schema.examples && schema.examples.length > 0
-    ? `Possible Examples: ${schema.examples}`
-    : '';
+  if (schema.examples && schema.examples.length == 1) {
+    return `Possible Example: ${schema.examples}`;
+  } else if (schema.examples && schema.examples.length > 1) {
+    return `Possible Example: ${schema.examples}`;
+  } else {
+    return '';
+  }
 }
