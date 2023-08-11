6565cfc658510f06f809764b268fdc478ce13be8
PaulBredl <paulbredl@gmx.de>
2023-08-11 12:08:05 +0200

    remove unused class

--- frontend/config-assistant/src/helpers/propertyTooltipGenerator.ts
+++
@@ -1,7 +0,0 @@
-import type {ConfigTreeNodeData} from '@/model/ConfigDataTreeNode';
-import {describeSchema} from '@/helpers/schema/schemaDescriptor';
-
-export function generateTooltipText(nodeData: ConfigTreeNodeData) {
-  return `<div>
-      ${describeSchema(nodeData.schema, '' + nodeData.name, nodeData.parentSchema, true)}</div>`;
-}
