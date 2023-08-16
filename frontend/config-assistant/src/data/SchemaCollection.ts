17da71246ad2d5e2c139e9ec49c3db3825226254
Keyuri_Patel <129445010+Keyshal-211@users.noreply.github.com>
2023-08-16 11:13:56 +0200

    adding more 3 example schemas (#229)
    * adding more 3 example schemas
    
    * editing version
    
    * change schema

--- frontend/config-assistant/src/data/SchemaCollection.ts
+++ frontend/config-assistant/src/data/SchemaCollection.ts
@@ -1,7 +1,13 @@
 import {DEFAULT_SCHEMA} from '@/data/DefaultSchema';
 import {ANIMAL_SCHEMA} from '@/data/AnimalSchema';
+import {STRENDA_SCHEMA} from '@/data/StrendaSchema';
+import {ENZYMEML_SCHEMA} from '@/data/EnzymemlSchema';
+import {AUTONOMOUS_VEHICLE_SCHEMA} from '@/data/AutonomousVehicleSchema';
 
 export const schemaCollection = [
   {label: 'Default Schema', key: 'default', schema: DEFAULT_SCHEMA},
   {label: 'Animal Schema', key: 'animal', schema: ANIMAL_SCHEMA},
+  {label: 'Strenda Schema', key: 'strenda', schema: STRENDA_SCHEMA},
+  {label: 'Enzymeml Schema', key: 'enzymeml', schema: ENZYMEML_SCHEMA},
+  {label: 'Autonomous Vehicle Schema', key: 'autonomousvehical', schema: AUTONOMOUS_VEHICLE_SCHEMA},
 ];
