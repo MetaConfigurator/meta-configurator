5f8f95f4306e6e5cfb8e41493c8d74a93bcdebb0
Keyshal-211 <kkpatel.de@gmail.com>
2023-08-16 10:59:37 +0200

    change schema

--- frontend/config-assistant/src/data/AutonomousVehicleSchema.ts
+++ frontend/config-assistant/src/data/AutonomousVehicleSchema.ts
@@ -1,34 +1,85 @@
 import type {TopLevelSchema} from '@/model/JsonSchemaType';
 
 export const AUTONOMOUS_VEHICLE_SCHEMA: TopLevelSchema = {
-  $schema: 'http://json-schema.org/draft/2020-12/schema',
+  $schema: 'https://json-schema.org/draft/2020-12/schema',
   title: 'Self-Driving Vehicle',
   description: 'A JSON schema for configuration of autonomous vehicles',
   type: 'object',
+  $defs: {
+    Point: {
+      title: 'Point in the 2D plane',
+      description: 'Position in absolute coordinates',
+      type: 'object',
+      properties: {
+        x: {
+          type: 'number',
+          description: 'X-coordinate of the starting location.',
+        },
+        y: {
+          type: 'number',
+          description: 'Y-coordinate of the starting location.',
+        },
+      },
+      required: ['x', 'y'],
+      additionalProperties: false,
+    },
+    WaypointReference: {
+      type: 'string',
+      title: 'Name of a waypoint',
+      description: 'Must a waypoint defined in the set of waypoints.',
+      pattern: '^[A-Z][a-z]+[0-9]*$',
+    },
+    SensorDefinition: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+          description: 'Name of the radar sensor.',
+          pattern: 'Sensor(0[1-9]|1[0-9]|20)$',
+          title: 'SensorName',
+        },
+        range: {
+          type: 'number',
+          description: 'Maximum range of the sensor. Unit in mm',
+          examples: ['400'],
+          title: 'SensorRange',
+        },
+        position: {
+          description: "Position of the radar sensor relative to the vehicle's center.",
+          $ref: '#/$defs/Point',
+          title: 'SensorPosition',
+        },
+      },
+      required: ['name', 'range', 'position'],
+      additionalProperties: false,
+    },
+  },
+  additionalProperties: false,
   properties: {
+    SimulationName: {
+      type: 'string',
+      description: 'Name of the simulation.',
+      pattern: '^Sim_.',
+      title: 'SimulationName',
+    },
     SelfDrivingVehicle: {
       type: 'object',
       properties: {
-        StartLocation: {
-          type: 'object',
+        StartingLocation: {
+          title: 'Starting Location',
           description: 'The starting location of the self-driving vehicle.',
-          properties: {
-            x: {
-              type: 'number',
-              description: 'X-coordinate of the starting location.',
-            },
-            y: {
-              type: 'number',
-              description: 'Y-coordinate of the starting location.',
-            },
-          },
-          required: ['x', 'y'],
-          additionalProperties: false,
+          $ref: '#/$defs/Point',
+        },
+        Destination: {
+          title: 'Destination',
+          description: 'The destination of the self-driving vehicle.',
+          $ref: '#/$defs/Point',
         },
         PlanningAlgorithm: {
           type: 'string',
           description: 'The algorithms used for route planning.',
           enum: ['A* search', 'Dijkstra', 'Rapidly Random Tree (RRT)'],
+          title: 'PlanningAlgorithm',
         },
         SensorSet: {
           type: 'object',
@@ -38,50 +89,21 @@ export const AUTONOMOUS_VEHICLE_SCHEMA: TopLevelSchema = {
               type: 'array',
               description: 'Array of radar sensor properties.',
               items: {
-                type: 'object',
-                properties: {
-                  SensorName: {
-                    type: 'string',
-                    description: 'Name of the radar sensor.',
-                    pattern: '^Radar_Sensor(0[1-9]|1[0-9]|20)$',
-                  },
-                  SensorRange: {
-                    type: 'number',
-                    description: 'Maximum range of the sensor. Unit in mm',
-                    examples: ['400'],
-                  },
-                  SensorPosition: {
-                    type: 'object',
-                    description: "Position of the radar sensor relative to the vehicle's center.",
-                    properties: {
-                      x: {
-                        type: 'number',
-                        description: 'X-coordinate position.',
-                      },
-                      y: {
-                        type: 'number',
-                        description: 'Y-coordinate position.',
-                      },
-                    },
-                    required: ['x', 'y'],
-                  },
-                },
-                required: ['name', 'range', 'position'],
-                additionalProperties: false,
+                $ref: '#/$defs/SensorDefinition',
               },
             },
             Lidar: {
               type: 'array',
               description: 'Array of lidar sensor properties.',
               items: {
-                $ref: '#/properties/SelfDrivingVehicle/properties/SensorSet/properties/Radar/items',
+                $ref: '#/$defs/SensorDefinition',
               },
             },
             Ultrasonic: {
               type: 'array',
               description: 'Array of ultrasonic sensor properties.',
               items: {
-                $ref: '#/properties/SelfDrivingVehicle/properties/SensorSet/properties/Radar/items',
+                $ref: '#/$defs/SensorDefinition',
               },
             },
           },
@@ -89,32 +111,33 @@ export const AUTONOMOUS_VEHICLE_SCHEMA: TopLevelSchema = {
         },
         VehicleType: {
           type: 'string',
-          description: 'Levels of the Self-driving vehicle.',
+          description:
+            'Levels of the Self-driving vehicle. Level 1 is the lowest level of automation and Level 6 is the highest level of automation.',
           enum: ['Level 1', 'Level 2', 'Level 3', 'Level 4', 'Level 5', 'Level 6'],
         },
         PassengerCapacity: {
           type: 'integer',
           description: 'Maximum number of passengers the vehicle can carry.',
           minimum: 1,
-          maximum: 10,
-          exclusiveMaximum: true,
+          exclusiveMaximum: 10,
+          title: 'Passenger capacity',
         },
         MaxSpeed: {
           type: 'integer',
-          description: 'Maximum speed of the vehicle.',
+          description: 'Maximum speed of the vehicle. Unit in km/h. Must be a multiple of 10.',
           minimum: 0,
           maximum: 480,
+          multipleOf: 10,
         },
         'is4-Wheel-Drive': {
           type: 'boolean',
           description: 'if the car is a 4 wheel driven model',
-        },
-        ProducedIn: {
-          type: 'string',
-          const: 'Germany',
+          title: 'is4-Wheel-Drive',
+          default: false,
         },
       },
       required: ['StartingLocation', 'PlanningAlgorithm', 'SensorSet', 'VehicleType'],
+      title: 'SelfDrivingVehicle',
     },
     Environment: {
       type: 'object',
@@ -123,65 +146,108 @@ export const AUTONOMOUS_VEHICLE_SCHEMA: TopLevelSchema = {
           type: 'string',
           description: 'Current weather conditions',
           enum: ['sunny', 'rainy', 'cloudy'],
+          title: 'Weather',
         },
         Temperature: {
           type: 'number',
           description: 'Current temperature in Celsius.',
+          title: 'Temperature',
         },
         Humidity: {
           type: 'integer',
           description: 'Relative humidity as a percentage.',
           minimum: 0,
           maximum: 100,
+          title: 'Humidity',
         },
       },
       required: ['Weather'],
-      additionalProperties: true,
+      additionalProperties: {
+        type: 'string',
+        description: 'Additional environment properties.',
+      },
     },
     Vehicles: {
       type: 'object',
-      properties: {
-        VehicleType: {
-          $ref: '#/properties/SelfDrivingVehicle/properties/VehicleType',
-        },
-        PathToDrive: {
-          type: 'array',
-          description: "List of coordinates representing the vehicle's path.",
-          items: {
+      description: 'Set of vehicles.',
+      propertyNames: {
+        type: 'string',
+        minLength: 1,
+        maxLength: 20,
+        pattern: '^[a-z]+[0-9]*$',
+      },
+      additionalProperties: {
+        type: 'object',
+        properties: {
+          VehicleType: {
+            type: 'string',
+            enum: ['car', 'truck', 'bus'],
+            description: 'Type of the vehicle.',
+            default: 'car',
+          },
+          PathToDrive: {
+            type: 'array',
+            description: "List of coordinates representing the vehicle's path.",
+            items: {
+              $ref: '#/$defs/WaypointReference',
+            },
+            additionalProperties: false,
+            title: 'PathToDrive',
+          },
+          DrivingSpeed: {
+            type: 'number',
+            description: 'Average driving speed of the vehicle.',
+            minimum: 0,
+            maximum: 480,
+            title: 'Driving Speed',
+          },
+          IsElectric: {
+            type: 'boolean',
+            description: 'if the car is an electric model',
+            default: false,
+          },
+          DriverBehavior: {
             type: 'object',
+            description: 'Driver behavior of the vehicle.',
             properties: {
-              x: {
+              AggressivenessFactor: {
                 type: 'number',
-                description: 'X-coordinate of the point.',
+                description:
+                  'Aggressiveness factor of the driver. This influences e.g. the acceleration and deceleration of the vehicle and the likelihood of overtaking other vehicles.',
                 minimum: 0,
+                maximum: 1,
               },
-              y: {
+              ReactionTime: {
                 type: 'number',
-                description: 'Y-coordinate of the point.',
+                description:
+                  'Reaction time of the driver in ms. This influences e.g. the reaction time to obstacles and traffic lights.',
                 minimum: 0,
+                maximum: 1000,
+              },
+              Distraction: {
+                type: 'number',
+                description:
+                  'Distraction factor of the driver. This influences e.g. the likelihood of the driver being distracted by a phone call or a passenger.',
+                minimum: 0,
+                maximum: 1,
+              },
+              Fatigue: {
+                type: 'number',
+                description:
+                  'Fatigue factor of the driver. This influences e.g. the likelihood of the driver falling asleep.',
+                minimum: 0,
+                maximum: 1,
               },
             },
-            required: ['x', 'y'],
           },
-          additionalProperties: false,
         },
-        DrivingSpeed: {
-          type: 'number',
-          description: 'Average driving speed of the vehicle.',
-          minimum: 0,
-          maximum: 480,
-        },
-        FuelType: {
-          type: 'string',
-          description: 'Type of fuel used by the vehicle.',
-          enum: ['Gasoline', 'Diesel', 'Electric', 'Gas'],
-        },
+        required: ['VehicleType', 'PathToDrive', 'DrivingSpeed'],
+        additionalProperties: false,
       },
-      required: ['VehicleType', 'PathToDrive', 'DrivingSpeed'],
     },
-    Pedestrians: {
+    PedestrianGroups: {
       type: 'array',
-      description: 'Array of pedestrian objects.',
+      description: 'Array of pedestrian groups.',
       items: {
         type: 'object',
         properties: {
@@ -193,37 +259,96 @@ export const AUTONOMOUS_VEHICLE_SCHEMA: TopLevelSchema = {
           Count: {
             type: 'integer',
             description: 'Number of pedestrians.',
+            default: 1,
+            minimum: 1,
+            maximum: 1000,
           },
           AgeGroup: {
             type: 'string',
             description: 'Age group of pedestrians.',
             enum: ['child', 'adult', 'elderly'],
+            default: 'adult',
+          },
+          Path: {
+            type: 'array',
+            description: "List of coordinates representing the pedestrian's path.",
+            items: {
+              $ref: '#/$defs/WaypointReference',
+            },
+            minItems: 1,
+          },
+          Speed: {
+            type: 'number',
+            description: 'Average walking speed of the pedestrians in km/h.',
+            minimum: 0,
+            maximum: 10,
           },
         },
-        required: ['Distribution', 'Count'],
+        additionalProperties: false,
+        required: ['Path', 'Speed'],
+      },
+    },
+    Waypoints: {
+      type: 'object',
+      description:
+        'Set of waypoints. Each waypoint is defined by a name and a position. The name must consist of a capital letter followed by at least one lowercase letter and optionally a number.',
+      propertyNames: {
+        type: 'string',
+        minLength: 1,
+        maxLength: 20,
+        pattern: '^[A-Z][a-z]+[0-9]*$',
+      },
+      additionalProperties: {
+        type: 'object',
+        description: 'Position of the waypoint.',
+        properties: {
+          point: {
+            $ref: '#/$defs/Point',
+          },
+          connectedWaypoints: {
+            type: 'array',
+            description: 'List of edges connected to the waypoint.',
+            items: {
+              type: 'string',
+              description: 'Name of the connected waypoint.',
+              pattern: '^[A-Z][a-z]+[0-9]*$',
+            },
+          },
+          waypointType: {
+            type: 'string',
+            description: 'Type of the waypoint.',
+            enum: [
+              'street',
+              'sidewalk',
+              'bikeLane',
+              'trafficLight',
+              'stopSign',
+              'pedestrianCrossing',
+            ],
+            default: 'normal',
+          },
+        },
+        required: ['point'],
+        additionalProperties: false,
       },
     },
     SimulationSettings: {
       type: 'object',
       properties: {
-        TimeUnit: {
-          type: 'string',
-          description: 'Time unit for simulation duration.',
-          enum: ['seconds', 'minutes', 'hours'],
-        },
         Duration: {
           type: 'number',
           description: 'Duration of the simulation in the specified time unit.',
           minimum: 0,
         },
-        SimulationName: {
+        TimeUnit: {
           type: 'string',
-          description: 'Name of the simulation.',
-          pattern: '^Sim_.',
+          description: 'Time unit for simulation duration.',
+          enum: ['seconds', 'minutes', 'hours'],
         },
       },
       required: ['TimeUnit', 'Duration'],
       additionalProperties: false,
+      title: 'Other simulation settings',
     },
   },
 };
