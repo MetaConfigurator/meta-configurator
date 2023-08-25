935628a7b8a10ae0fae6c99aaca80a3dc01c153b
Keyshal-211 <kkpatel.de@gmail.com>
2023-08-25 15:53:49 +0200

    adding try catch

--- frontend/config-assistant/src/create-cookies/cookiesHandler.ts
+++ frontend/config-assistant/src/create-cookies/cookiesHandler.ts
@@ -10,15 +10,18 @@ const cookiesHandler = {
     const schemaDataCookie = VueCookies.get('schemaData');
     const settingsDataCookie = VueCookies.get('settingsData');
     const fileDataCookie = VueCookies.get('fileData');
-
-    if (schemaDataCookie && schemaDataCookie !== 'undefined') {
-      useDataStore().schemaData = JSON.parse(schemaDataCookie);
-    }
-    if (fileDataCookie && fileDataCookie !== 'undefined') {
-      useDataStore().fileData = JSON.parse(fileDataCookie);
-    }
-    if (settingsDataCookie && settingsDataCookie !== 'undefined') {
-      useSettingsStore().settingsData = JSON.parse(settingsDataCookie);
+    try {
+      if (schemaDataCookie && schemaDataCookie !== 'undefined') {
+        useDataStore().schemaData = JSON.parse(schemaDataCookie);
+      }
+      if (fileDataCookie && fileDataCookie !== 'undefined') {
+        useDataStore().fileData = JSON.parse(fileDataCookie);
+      }
+      if (settingsDataCookie && settingsDataCookie !== 'undefined') {
+        useSettingsStore().settingsData = JSON.parse(settingsDataCookie);
+      }
+    } catch (error) {
+      console.error('Error parsing cookies:', error);
     }
 
     // Watch for changes in schemaData, settingsData, and fileData and update cookies
@@ -29,6 +32,10 @@ const cookiesHandler = {
         () => useDataStore().fileData,
       ],
       ([newSchemaData, newSettingsData, newFileData]) => {
+        console.log('schemaDataCookie:', schemaDataCookie);
+        console.log('settingsDataCookie:', settingsDataCookie);
+        console.log('fileDataCookie:', fileDataCookie);
+
         VueCookies.set('schemaData', JSON.stringify(newSchemaData));
         VueCookies.set('settingsData', JSON.stringify(newSettingsData));
         VueCookies.set('fileData', JSON.stringify(newFileData));
