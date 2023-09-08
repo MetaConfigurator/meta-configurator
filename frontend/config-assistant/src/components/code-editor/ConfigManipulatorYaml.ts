66cb2609920ed3f7b6be04d9c992dc02ae5d3fb9
beyond2void <minyexu@gmail.com>
2023-09-08 10:48:53 +0200

    current state of yaml support

--- frontend/config-assistant/src/components/code-editor/ConfigManipulatorYaml.ts
+++ frontend/config-assistant/src/components/code-editor/ConfigManipulatorYaml.ts
@@ -1,6 +1,8 @@
-import type {Path} from '@/model/path';
+import type {Path, PathElement} from '@/model/path';
 import type {ConfigManipulator} from '@/components/code-editor/ConfigManipulator';
 import YAML from 'yaml';
+import {Parser} from 'yaml';
+import {errorService} from '@/main';
 
 export class ConfigManipulatorYaml implements ConfigManipulator {
   constructor() {}
@@ -13,8 +15,41 @@ export class ConfigManipulatorYaml implements ConfigManipulator {
     return YAML.stringify(content);
   }
   determineCursorPosition(editorContent: string, currentPath: Path): number {
-    return 0;
-    // TODO
+    try {
+      const document = new Parser().parse(editorContent);
+      return this.determineCursorPositionStep(document, currentPath);
+    } catch (e) {
+      errorService.onError(e);
+      return 0;
+    }
+  }
+
+  determineCursorPositionStep(currentNode: any, currentPath: Path): number {
+    if (!currentNode) return 0;
+
+    if (currentPath.length === 0) {
+      return currentNode.range ? currentNode.range[1] : 0;
+    }
+
+    const nextKey: PathElement = currentPath[0];
+
+    if (currentNode.type === 'object') {
+      for (const item of currentNode.items) {
+        if (item.key && item.key.value === nextKey) {
+          return this.determineCursorPositionStep(item.value, currentPath.slice(1));
+        }
+      }
+    } else if (currentNode.type === 'array') {
+      let index = 0;
+      for (const item of currentNode.items) {
+        if (index === nextKey) {
+          return this.determineCursorPositionStep(item, currentPath.slice(1));
+        }
+        index++;
+      }
+    }
+
+    return currentNode.range ? currentNode.range[0] : 0;
   }
 
   determinePath(editorContent: string, targetCharacter: number): Path {
