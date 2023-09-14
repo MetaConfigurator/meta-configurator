120002bde0fe5b81c770e0c774b6107c48a5bfdc
Felix Neubauer <felix@neuby.de>
2023-09-14 17:45:11 +0200

    now automatically apply constants on oneOf schema selection

--- frontend/config-assistant/src/components/gui-editor/properties/OneOfOfSelectionProperty.vue
+++ frontend/config-assistant/src/components/gui-editor/properties/OneOfOfSelectionProperty.vue
@@ -6,6 +6,9 @@ import {useSessionStore} from '@/store/sessionStore';
 import {Path, PathElement} from '@/model/path';
 import {pathToString} from '@/helpers/pathHelper';
 import {OneOfAnyOfSelectionOption, schemaOptionToString} from '@/model/OneOfAnyOfSelectionOption';
+import type {JsonSchemaObjectType} from '@/model/JsonSchemaType';
+import {safeMergeSchemas} from '@/helpers/schema/mergeAllOfs';
+import _ from 'lodash';
 
 const props = defineProps<{
   propertyName: PathElement;
@@ -21,6 +24,7 @@ const possibleValues = props.possibleSchemas.map(
 
 const emit = defineEmits<{
   (e: 'update_tree'): void;
+  (e: 'update_property_value', newValue: any | undefined): void;
 }>();
 
 const valueProperty = computed({
@@ -31,14 +35,62 @@ const valueProperty = computed({
 
   set(newValue) {
     const selectedOption: OneOfAnyOfSelectionOption | undefined = newValue;
+    const path = pathToString(props.absolutePath);
 
     if (selectedOption) {
-      const path = pathToString(props.absolutePath);
       useSessionStore().currentSelectedOneOfOptions.set(path, selectedOption);
+      applySchemaConstantsOnDataBasedOnSelection(props.absolutePath, selectedOption);
+      // TODO: store merged Schema in session store instead of recompute in TreeNodeResolver
+      emit('update_tree');
+    } else {
+      useSessionStore().currentSelectedOneOfOptions.delete(path);
       emit('update_tree');
     }
   },
 });
+
+function applySchemaConstantsOnDataBasedOnSelection(
+  path: Path,
+  selectedOneOfOption: OneOfAnyOfSelectionOption
+) {
+  const schemaAtPath = useSessionStore().schemaAtPath(path);
+  console.log(
+    'apply schema constants ',
+    path,
+    selectedOneOfOption,
+    schemaAtPath.jsonSchema,
+    'oneOf ',
+    schemaAtPath.oneOf[selectedOneOfOption.index]
+  );
+  const baseSchema = {...schemaAtPath.jsonSchema};
+  delete baseSchema.oneOf;
+  const mergedSchema = safeMergeSchemas(
+    baseSchema,
+    schemaAtPath.oneOf[selectedOneOfOption.index].jsonSchema
+  );
+  console.log('apply schema constants merged schema ', path, mergedSchema);
+  const resultData = applySchemaConstantsOnData(mergedSchema, props.propertyData);
+  if (!_.isEqual(resultData, props.propertyData)) {
+    emit('update_property_value', resultData);
+  }
+}
+function applySchemaConstantsOnData(schema: JsonSchemaObjectType, data: any): any {
+  // note that in pre-processing all const is converted to an enum with just one entry
+  if (schema.enum) {
+    console.log('apply schema constants merged schema is enum', schema, {...data});
+    if (schema.enum.length == 1) {
+      data = schema.enum[0];
+      console.log('change data', schema, data);
+    }
+  }
+  if (schema.properties) {
+    for (let key: string in schema.properties) {
+      const propertySchema = schema.properties[key];
+      applySchemaConstantsOnData(propertySchema, data[key]);
+    }
+  }
+  return data;
+}
 </script>
 
 <template>
