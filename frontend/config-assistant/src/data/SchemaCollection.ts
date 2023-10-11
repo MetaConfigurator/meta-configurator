71d545848c4595ca20483d2ae290e1e702635f38
Keyshal-211 <kkpatel.de@gmail.com>
2023-10-11 13:33:30 +0200

    removing schema and changing name for default schema

--- frontend/config-assistant/src/data/SchemaCollection.ts
+++ frontend/config-assistant/src/data/SchemaCollection.ts
@@ -1,13 +1,11 @@
 import {DEFAULT_SCHEMA} from '@/data/DefaultSchema';
-import {ANIMAL_SCHEMA} from '@/data/AnimalSchema';
 import {STRENDA_SCHEMA} from '@/data/StrendaSchema';
 import {ENZYMEML_SCHEMA} from '@/data/EnzymemlSchema';
 import {AUTONOMOUS_VEHICLE_SCHEMA} from '@/data/AutonomousVehicleSchema';
 import type {SchemaOption} from '@/model/SchemaOption';
 
 export const schemaCollection: SchemaOption[] = [
-  {label: 'Default Schema', key: 'default', schema: DEFAULT_SCHEMA},
-  {label: 'Animal Schema', key: 'animal', schema: ANIMAL_SCHEMA},
+  {label: 'Feature testing Schema', key: 'default', schema: DEFAULT_SCHEMA},
   {label: 'Strenda Schema', key: 'strenda', schema: STRENDA_SCHEMA},
   {label: 'Enzymeml Schema', key: 'enzymeml', schema: ENZYMEML_SCHEMA},
   {label: 'Autonomous Vehicle Schema', key: 'autonomousvehical', schema: AUTONOMOUS_VEHICLE_SCHEMA},
