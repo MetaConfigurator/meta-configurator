5ca0bf829c18040261593edc1ad7b2371f918dec
Felix Neubauer <felix@neuby.de>
2024-06-14 13:50:47 +0200

    test if removing base path fixes resource not found in package

--- meta_configurator/vite.config.ts
+++ meta_configurator/vite.config.ts
@@ -6,7 +6,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/meta-configurator/',
   plugins: [vue(), vueJsx()],
   optimizeDeps: {
     exclude: ['ajv-formats'],
