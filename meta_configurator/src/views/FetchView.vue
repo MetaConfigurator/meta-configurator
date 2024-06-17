3ccfca53ba071be027bfa38b5ec23523f97e826b
Felix Neubauer <33369081+Logende@users.noreply.github.com>
2024-06-17 12:22:17 +0200

    Feature/nginx and docker fixes and extended log messages (#501)
    * attempt adding dockerization and nginx
    
    * add docker compose file
    
    * update nginx config, attempting to fix resource not found in deployed application
    
    * update nginx config, attempting to fix resource not found in deployed application
    
    * update nginx config, attempting to fix resource not found in deployed application
    
    * test if removing base path fixes resource not found in package
    
    * change node version to 16 and deactivate minify for debugging
    
    * adjust nginx.conf, attempting to fix issue
    
    * try fix MIME type
    
    * attempt fix issue in browser
    
    * replace usage of crypto by math.random for random id
    
    * make vite.config use base path for github pages but not otherwise
    
    * fix query parameter for data
    
    * remove header, hoping this will lead to raw json when fetching
    
    * add more debugging messages
    
    * apply formatting changes
    
    * update debugging: now always print query string itself
    
    * apply formatting changes
    
    * fix debug output
    
    * disable minify
    
    * apply formatting changes
    
    * make dockerize workflow checkout branch
    
    ---------
    
    Co-authored-by: Logende <Logende@users.noreply.github.com>

--- meta_configurator/src/views/FetchView.vue
+++ meta_configurator/src/views/FetchView.vue
@@ -14,13 +14,13 @@ onMounted(() => {
 
   if ('settings' in query) {
     const settings_url = query.settings as string;
-    console.debug('Received settings URL ', settings_url, ' from query.');
+    console.info('Received settings URL ', settings_url, ' from query string "', query, '".');
     fetch(settings_url)
       .then(function (response) {
         return response.json();
       })
       .then(function (jsonData) {
-        console.debug('Fetched provided settings file and parsed as json.', jsonData);
+        console.info('Fetched provided settings file and parsed as json.', jsonData);
         useDataSource().settingsData.value = jsonData;
       });
   }
@@ -30,27 +30,27 @@ onMounted(() => {
     useSessionStore().hasShownInitialDialog = true;
 
     const schema_url = query.schema as string;
-    console.debug('Received schema URL ', schema_url, ' from query.');
+    console.info('Received schema URL ', schema_url, ' from query string "', query, '".');
     fetch(schema_url)
       .then(function (response) {
         return response.json();
       })
       .then(function (jsonData) {
-        console.debug('Fetched provided schema file and parsed as json.', jsonData);
+        console.info('Fetched provided schema file and parsed as json.', jsonData);
         useDataSource().userSchemaData.value = jsonData;
         // skip dialog if schema was already selected
       });
   }
 
   if ('data' in query) {
-    const data_url = query.file as string;
-    console.debug('Received data URL ', data_url, ' from query.');
+    const data_url = query.data as string;
+    console.info('Received data URL ', data_url, ' from query string "', query, '".');
     fetch(data_url)
       .then(function (response) {
         return response.json();
       })
       .then(function (jsonData) {
-        console.debug('Fetched provided file and parsed as json.', jsonData);
+        console.info('Fetched provided file and parsed as json.', jsonData);
         useDataSource().userData.value = jsonData;
       });
   }
