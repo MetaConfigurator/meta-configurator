b5e1bbebd4910750975319472685c3e3d5b0a67d
Felix Neubauer <33369081+Logende@users.noreply.github.com>
2024-07-31 18:22:24 +0800

    Feature/restart docker automatically (#528)
    * restart docker services automatically
    
    * apply formatting changes
    
    ---------
    
    Co-authored-by: Logende <Logende@users.noreply.github.com>

--- meta_configurator/src/components/panels/schema-diagram/SchemaObjectNode.vue
+++ meta_configurator/src/components/panels/schema-diagram/SchemaObjectNode.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {
   SchemaElementData,
+  SchemaObjectAttributeData,
   SchemaObjectNodeData,
 } from '@/components/panels/schema-diagram/schemaDiagramTypes';
 import SchemaObjectAttribute from '@/components/panels/schema-diagram/SchemaObjectAttribute.vue';
@@ -21,6 +22,12 @@ const emit = defineEmits<{
   (e: 'select_element', path: Path): void;
   (e: 'zoom_into_element', path: Path): void;
   (e: 'update_object_name', objectData: SchemaElementData, oldName: string, newName: string): void;
+  (
+    e: 'update_attribute_name',
+    attributeData: SchemaObjectAttributeData,
+    oldName: string,
+    newName: string
+  ): void;
 }>();
 
 const objectName = ref(props.data.name);
@@ -46,6 +53,13 @@ function updateObjectName() {
     return;
   }
   emit('update_object_name', props.data, props.data.name, newName);
+}
+function updateAttributeName(
+  attributeData: SchemaObjectAttributeData,
+  oldName: string,
+  newName: string
+) {
+  emit('update_attribute_name', attributeData, oldName, newName);
 }
 
 function isHighlighted() {
@@ -78,7 +92,8 @@ function isHighlighted() {
       v-for="attribute in props.data!.attributes"
       :data="attribute!"
       :selected-data="props.selectedData"
-      @select_element="clickedAttribute"></SchemaObjectAttribute>
+      @select_element="clickedAttribute"
+      @update_attribute_name="updateAttributeName"></SchemaObjectAttribute>
     <Handle
       id="main"
       type="source"
