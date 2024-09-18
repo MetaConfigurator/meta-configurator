13574d882f109a1adb698cb23bd9c6eecca2292c
Felix Neubauer <felix@neuby.de>
2024-09-18 15:01:54 +0200

    add utility for url parameters that for github links the raw link is resolved already

--- meta_configurator/src/views/FetchView.vue
+++ meta_configurator/src/views/FetchView.vue
@@ -9,12 +9,31 @@ defineProps({
   settings_url: String,
 });
 
+
+function processUrl(url: string) : string {
+  // if url is Github URL, convert to raw source code URL
+  if (url.includes('github.com')) {
+    const parts = url.split('/');
+    if (parts.length < 8) {
+      console.error('Invalid Github URL: ', url);
+      return url;
+    }
+    const user = parts[3];
+    const repo = parts[4];
+    const branch = parts[6];
+    const path = parts.slice(7).join('/');
+    return `https://raw.githubusercontent.com/${user}/${repo}/${branch}/${path}`;
+  }
+
+  return url;
+}
+
 onMounted(() => {
   const route = useAppRouter().currentRoute.value;
   const query = route.query;
 
   if ('settings' in query) {
-    const settingsUrl = query.settings as string;
+    const settingsUrl = processUrl(query.settings as string);
     console.info('Received settings URL ', settingsUrl, ' from query string "', query, '".');
     fetch(settingsUrl)
       .then(function (response) {
@@ -30,7 +49,7 @@ onMounted(() => {
     console.debug('skip initial schema selection dialog');
     useSessionStore().hasShownInitialDialog = true;
 
-    const schemaUrl = query.schema as string;
+    const schemaUrl = processUrl(query.schema as string);
     console.info('Received schema URL ', schemaUrl, ' from query string "', query, '".');
     fetch(schemaUrl)
       .then(function (response) {
@@ -44,7 +63,7 @@ onMounted(() => {
   }
 
   if ('data' in query) {
-    const dataUrl = query.data as string;
+    const dataUrl = processUrl(query.data as string);
     console.info('Received data URL ', dataUrl, ' from query string "', query, '".');
     fetch(dataUrl)
       .then(function (response) {
