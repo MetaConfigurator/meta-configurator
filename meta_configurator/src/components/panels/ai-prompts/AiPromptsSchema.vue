e091145a5479d3bc92718ae1db98f11607ded671
Felix Neubauer <33369081+Logende@users.noreply.github.com>
2025-01-27 19:20:54 +0100

    implement AI export feature (#604)
    * implement AI export feature
    
    * apply formatting changes
    
    ---------
    
    Co-authored-by: Logende <Logende@users.noreply.github.com>

--- meta_configurator/src/components/panels/ai-prompts/AiPromptsSchema.vue
+++ meta_configurator/src/components/panels/ai-prompts/AiPromptsSchema.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import AiPromptsTemplate from '@/components/panels/ai-prompts/AiPromptsTemplate.vue';
-import {querySchemaCreation, querySchemaModification, querySchemaQuestion} from '@/utility/openai';
+import {
+  queryDataConversionFromJson,
+  querySchemaCreation,
+  querySchemaModification,
+  querySchemaQuestion,
+} from '@/utility/openai';
 import {SessionMode} from '@/store/sessionMode';
 
 const props = defineProps<{
@@ -28,6 +33,15 @@ function queryDocumentQuestion(
 ): Promise<string> {
   return querySchemaQuestion(apiKey, prompt, currentData);
 }
+
+function queryDocumentExport(
+  apiKey: string,
+  prompt: string,
+  currentData: string,
+  schema: string
+): Promise<string> {
+  return queryDataConversionFromJson(apiKey, prompt, currentData, schema);
+}
 </script>
 
 <template>
@@ -36,11 +50,13 @@ function queryDocumentQuestion(
     default-text-create-document="Enter your Schema description"
     default-text-modify-document="How do you want your Schema to be modified?"
     default-text-question-document="Ask a question about your Schema"
+    default-text-export-document="Enter an example file of your desired output Data Format or describe the Format."
     label-document-type="Schema"
     label-modify-info="When the complete schema is selected for modification, the complete schema will be processed by the AI to apply the modification. If you want a modification only for a specific class or attribute type, selecting that element will help reduce the processing time for the modification and increase the quality of the result. Especially for large schemas, it is not recommended to use the complete schema document for generating modifications."
     :function-query-document-creation="queryDocumentCreation"
     :function-query-document-modification="queryDocumentModification"
-    :function-query-document-question="queryDocumentQuestion" />
+    :function-query-document-question="queryDocumentQuestion"
+    :function-query-document-export="queryDocumentExport" />
 </template>
 
 <style scoped></style>
