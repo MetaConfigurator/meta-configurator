128e1bda08c6a0bc4d5abb1192879e6ca768fa80
Logende <Logende@users.noreply.github.com>
2025-02-09 17:49:58 +0000

    apply formatting changes

--- meta_configurator/src/setupTests.ts
+++ meta_configurator/src/setupTests.ts
@@ -1,13 +1,12 @@
-import { vi } from 'vitest'
+import {vi} from 'vitest';
 
-global.window.matchMedia = vi.fn().mockImplementation((query) => ({
-    matches: false, // Change to true if you want dark mode by default
-    media: query,
-    onchange: null,
-    addListener: vi.fn(), // Deprecated
-    removeListener: vi.fn(), // Deprecated
-    addEventListener: vi.fn(),
-    removeEventListener: vi.fn(),
-    dispatchEvent: vi.fn(),
+global.window.matchMedia = vi.fn().mockImplementation(query => ({
+  matches: false, // Change to true if you want dark mode by default
+  media: query,
+  onchange: null,
+  addListener: vi.fn(), // Deprecated
+  removeListener: vi.fn(), // Deprecated
+  addEventListener: vi.fn(),
+  removeEventListener: vi.fn(),
+  dispatchEvent: vi.fn(),
 }));
-
