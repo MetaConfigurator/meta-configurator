d69616cef3ceef66f81d16dadc042a663efcd73b
Felix Neubauer <felix@neuby.de>
2025-02-09 18:49:05 +0100

    fix tests

---
+++ meta_configurator/src/setupTests.ts
@@ -0,0 +1,13 @@
+import { vi } from 'vitest'
+
+global.window.matchMedia = vi.fn().mockImplementation((query) => ({
+    matches: false, // Change to true if you want dark mode by default
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // Deprecated
+    removeListener: vi.fn(), // Deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+}));
+
