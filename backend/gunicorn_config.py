b233600d831d929a83a2df7103b79cde38b7c649
Felix Neubauer <felix@neuby.de>
2025-02-13 12:17:02 +0100

    update gunicorn to use https

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,3 +1,7 @@
 bind = "0.0.0.0:5000"
 workers = 4
 timeout = 120
+
+# SSL Configuration
+certfile = "/app/local.crt"  # Path to the SSL certificate
+keyfile = "/app/local.key"  # Path to the SSL private key
