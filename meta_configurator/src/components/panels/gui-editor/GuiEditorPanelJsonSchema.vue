976a71eea5db14ccbe29610c908e812daea3845f
Felix Neubauer <felix@neuby.de>
2025-03-10 19:55:47 +0100

    fix issue with GUI not being synchronized with text editor, BUT this disables sorting new properties

--- meta_configurator/src/components/panels/gui-editor/GuiEditorPanelJsonSchema.vue
+++ meta_configurator/src/components/panels/gui-editor/GuiEditorPanelJsonSchema.vue
@@ -22,48 +22,6 @@ function updatePath(newPath: Path) {
 }
 
 function updateData(path: Path, newValue: any) {
-  if (path.length > 0) {
-    const dataAtPath = data.dataAt(path);
-
-    if (dataAtPath == null) {
-      // if the element is new, we add it to the parent object in a sorted way
-      const parentPath = path.slice(0, path.length - 1);
-      const parentSchemaPath = dataPathToSchemaPath(parentPath);
-      const parentSchemaProps = getSchemaForMode(props.sessionMode).effectiveSchemaAtPath(
-        parentSchemaPath
-      ).schema.properties;
-      const parentData = data.dataAt(parentPath);
-      if (!_.isEmpty(parentSchemaProps) && !_.isEmpty(parentData) && !Array.isArray(parentData)) {
-        // only proceed with property sorting when parent schema and data are not empty and the parent schema is of type object (not array!)
-        // warning: this function only works for normal properties, not for composition, conditionals and other advanced features
-        // for those advanced features, the new property will be added at the end of the object
-        // TODO: implement sorting for advanced features. This will be more complicated and will require a lot of testing
-        const newElementKey = path[path.length - 1];
-        parentData[newElementKey] = newValue; // Add the new property
-        const schemaKeys = Object.keys(parentSchemaProps);
-        const dataKeys = Object.keys(parentData);
-
-        // sort the document properties based on the order of schema properties
-        const sortedProperties: {[key: string]: any} = {};
-        schemaKeys.forEach(key => {
-          if (dataKeys.includes(key) || key === newElementKey) {
-            sortedProperties[key] = parentData[key];
-          }
-        });
-        // after adding properties from the schema in proper order, add the rest of the properties f
-        dataKeys.forEach(key => {
-          if (!schemaKeys.includes(key)) {
-            sortedProperties[key] = parentData[key];
-          }
-        });
-
-        // if properties were sorted, update the parent object instead of the new property
-        path = parentPath;
-        newValue = sortedProperties;
-      }
-    }
-  }
-
   data.setDataAt(path, newValue);
 }
 
