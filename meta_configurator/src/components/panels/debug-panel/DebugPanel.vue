b70181d1d21243b2dc6816a99c94e513c71acef3
Felix Neubauer <felix@neuby.de>
2025-03-19 18:03:37 +0100

    add schema definition extraction to debugging panel

--- meta_configurator/src/components/panels/debug-panel/DebugPanel.vue
+++ meta_configurator/src/components/panels/debug-panel/DebugPanel.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
 import {SessionMode} from '@/store/sessionMode';
 import DebugInfoForMode from '@/components/panels/debug-panel/DebugInfoForMode.vue';
+import Button from 'primevue/button';
+import {extractAllInlinedSchemaElements} from "@/schema/schemaManipulationUtils";
+import {getDataForMode} from "@/data/useDataLink";
+
+
+function extractAllSchemaElements() {
+  const schemaData = getDataForMode(SessionMode.SchemaEditor);
+  extractAllInlinedSchemaElements(schemaData, false, true)
+}
 </script>
 
 <template>
@@ -9,6 +18,7 @@ import DebugInfoForMode from '@/components/panels/debug-panel/DebugInfoForMode.v
       <DebugInfoForMode class="h-full" :sessionMode="SessionMode.DataEditor" />
       <DebugInfoForMode class="h-full" :sessionMode="SessionMode.SchemaEditor" />
       <DebugInfoForMode class="h-full" :sessionMode="SessionMode.Settings" />
+      <Button @click="extractAllSchemaElements" label="Extract all inlined schema elements" />
     </div>
   </div>
 </template>
