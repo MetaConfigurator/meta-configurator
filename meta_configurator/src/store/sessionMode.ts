e06897360f59d6191cdd17fb4c05802ba135380a
Felix Neubauer <felix@neuby.de>
2025-03-31 12:43:58 +0200

    make XML text editor read only in settings mode too

--- meta_configurator/src/store/sessionMode.ts
+++ meta_configurator/src/store/sessionMode.ts
@@ -3,3 +3,15 @@ export enum SessionMode {
   SchemaEditor = 'schemaEditor',
   Settings = 'settings',
 }
+
+
+export function modeToDocumentTypeDescription(mode: SessionMode): string {
+  switch (mode) {
+    case SessionMode.DataEditor:
+      return 'data';
+    case SessionMode.SchemaEditor:
+      return 'schema';
+    case SessionMode.Settings:
+      return 'settings';
+  }
+}
