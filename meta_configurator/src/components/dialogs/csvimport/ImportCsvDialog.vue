d8dd8cda12e9466bc9d7a0e13f328eb05b1a8129
Felix Neubauer <33369081+Logende@users.noreply.github.com>
2025-05-06 16:49:24 +0200

    make CSV import append instead of replace (#711)
    * make CSV import append instead of replace
    
    * apply formatting changes
    
    ---------
    
    Co-authored-by: Logende <Logende@users.noreply.github.com>

--- meta_configurator/src/components/dialogs/csvimport/ImportCsvDialog.vue
+++ meta_configurator/src/components/dialogs/csvimport/ImportCsvDialog.vue
@@ -224,11 +224,6 @@ defineExpose({show: openDialog, close: hideDialog});
 <template>
   <Dialog v-model:visible="showDialog" header="Import CSV">
     <div class="flex flex-wrap justify-content-center gap-3 bigger-dialog-content">
-      <p>
-        The import will insert the CSV data into your current document, overwriting existing data
-        for paths with a conflict.
-      </p>
-
       <div class="flex align-items-center">
         <Button
           label="Select CSV Document"
