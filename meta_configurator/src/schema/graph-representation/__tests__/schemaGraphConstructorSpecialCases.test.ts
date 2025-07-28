a456c134d5d7f4f29fdd140fe9c7f426686aa7e5
Felix Neubauer <felix@neuby.de>
2025-07-28 18:30:30 +0200

    extend and adapt tests for new enumeration fix for null/undefined values

--- meta_configurator/src/schema/graph-representation/__tests__/schemaGraphConstructorSpecialCases.test.ts
+++ meta_configurator/src/schema/graph-representation/__tests__/schemaGraphConstructorSpecialCases.test.ts
@@ -51,3 +51,47 @@ describe('tests for more difficult scenarios and special cases that result as a 
     expect(graph.nodes.length).toBe(3);
   });
 });
+
+
+
+describe('tests other special cases', () => {
+  it('anyOf with either one object or an array of these objects', () => {
+    let schema: TopLevelSchema = {
+      title: 'Workflow Schema',
+      type: 'object',
+      $defs: {
+        step: {
+          type: 'object',
+          properties: {
+            command: {type: 'string'},
+          },
+        },
+        stepArray: {
+          type: 'array',
+          items: {
+            $ref: '#/$defs/step',
+          },
+        },
+      },
+      properties: {
+        job: {
+          anyOf: [
+            {
+              $ref: '#/$defs/step',
+            },
+            {
+              $ref: '#/$defs/stepArray',
+            },
+          ],
+        },
+      },
+    };
+
+    const defs = identifyAllObjects(schema);
+    const graph = new SchemaGraph([], []);
+    populateGraph(defs, graph);
+    trimGraph(graph);
+
+    expect(graph.nodes.length).toBe(3);
+  });
+});
