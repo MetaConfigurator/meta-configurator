691fc4fa677f53cd5d5436393ce9f84efc53bfbe
Felix Neubauer <33369081+Logende@users.noreply.github.com>
2025-08-06 16:30:55 +0200

    add preCICE adapter config schema to examples (#804)
    * add preCICE adapter config schema to examples
    
    * apply formatting changes
    
    ---------
    
    Co-authored-by: Logende <Logende@users.noreply.github.com>

--- meta_configurator/src/packaged-schemas/schemaCollection.ts
+++ meta_configurator/src/packaged-schemas/schemaCollection.ts
@@ -11,5 +11,10 @@ export const schemaCollection: SchemaOption[] = [
   {label: 'Feature testing Schema', key: 'default', schema: DEFAULT_SCHEMA},
   {label: 'Strenda Schema', key: 'strenda', schema: STRENDA_SCHEMA},
   {label: 'Enzymeml Schema', key: 'enzymeml', schema: ENZYMEML_SCHEMA},
-  {label: 'Autonomous Vehicle Schema', key: 'autonomousvehical', schema: AUTONOMOUS_VEHICLE_SCHEMA},
+  {label: 'Autonomous Vehicle Schema', key: 'autonomousvehicle', schema: AUTONOMOUS_VEHICLE_SCHEMA},
+  {
+    label: 'preCICE Adapter Config Schema',
+    key: 'precice-adapter',
+    url: 'https://github.com/precice/preeco-orga/blob/main/adapter-config-schema/precice_adapter_config_schema.json',
+  },
 ];
