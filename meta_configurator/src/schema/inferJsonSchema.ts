12552a089df5084131bcd23c7fbc92f9742e6d41
Felix Neubauer <33369081+Logende@users.noreply.github.com>
2025-09-16 10:28:08 +0200

    improve schema inference to not output items: false schemas (#817)
    * improve schema inference to not output items: false schemas
    
    * apply formatting changes
    
    ---------
    
    Co-authored-by: Logende <Logende@users.noreply.github.com>

--- meta_configurator/src/schema/inferJsonSchema.ts
+++ meta_configurator/src/schema/inferJsonSchema.ts
@@ -9,5 +9,19 @@ export function inferJsonSchema(sampleData: any): JsonSchemaType {
   const minObjectPropertyCountToPreserve =
     useSettings().value.performance.minObjectPropertyCountToPreserve;
   sampleData = trimDataToMaxSize(sampleData, maximumSizeInKiB, minObjectPropertyCountToPreserve);
-  return inferSchema(sampleData).toJSONSchema();
+  return fixEmptyArraySchemas(inferSchema(sampleData).toJSONSchema());
+}
+
+function fixEmptyArraySchemas(schema: any): any {
+  // schemas inferred from empty arrays have "items": false, which is not very useful and can break downstream logic
+  // instead, we change it to "items": true, which means "any type"
+  if (schema && typeof schema === 'object') {
+    if (schema.type === 'array' && schema.items === false) {
+      schema.items = true; // means “any type”
+    }
+    for (const key of Object.keys(schema)) {
+      schema[key] = fixEmptyArraySchemas(schema[key]);
+    }
+  }
+  return schema;
 }
