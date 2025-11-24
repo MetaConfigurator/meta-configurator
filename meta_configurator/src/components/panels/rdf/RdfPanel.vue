b1ef083c5deb519f754f80f63407e2fcbe60493e
M-Jafarkhani <mahdi.jafarkhani@gmail.com>
2025-11-24 15:02:34 +0100

    Fix DataTable scrollheight

--- meta_configurator/src/components/panels/rdf/RdfPanel.vue
+++ meta_configurator/src/components/panels/rdf/RdfPanel.vue
@@ -1,34 +1,23 @@
 <template>
   <div class="panel-container">
-    <div class="rdf-panel">
-      <PanelSettings
-        panel-name="RDF View"
-        :panel-settings-path="['rdf']"
-        :sessionMode="SessionMode.DataEditor">
-      </PanelSettings>
-      <RmlMappingDialog ref="rmlMappingDialog" />
-      <div v-if="dataIsInJsonLd">
-        <RdfEditorPanel :sessionMode="props.sessionMode" @zoom_into_path="zoomIntoPath" />
-      </div>
-      <div v-else class="border border-yellow-400 bg-yellow-50 text-yellow-800 p-4 rounded mt-1">
-        To use RDF panel, your data should be in JSON-LD format. You can use
-        <a href="#" @click.prevent="showRmlMappingDialog" class="text-blue-600 hover:underline">
-          JSON to JSON-LD
-        </a>
-        utility to convert it to JSON-LD.
-      </div>
+    <PanelSettings
+      panel-name="RDF View"
+      :panel-settings-path="['rdf']"
+      :sessionMode="SessionMode.DataEditor">
+    </PanelSettings>
+    <RmlMappingDialog ref="rmlMappingDialog" />
+    <div class="panel-content" v-if="dataIsInJsonLd">
+      <RdfEditorPanel :sessionMode="props.sessionMode" @zoom_into_path="zoomIntoPath" />
+    </div>
+    <div v-else class="border border-yellow-400 bg-yellow-50 text-yellow-800 p-4 rounded mt-1">
+      To use RDF panel, your data should be in JSON-LD format. You can use
+      <a href="#" @click.prevent="showRmlMappingDialog" class="text-blue-600 hover:underline">
+        JSON to JSON-LD
+      </a>
+      utility to convert it to JSON-LD.
     </div>
   </div>
 </template>
-
-<style scoped>
-.rdf-panel {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-}
-</style>
 
 <script setup lang="ts">
 import {ref, watch} from 'vue';
@@ -134,3 +123,18 @@ function showRmlMappingDialog() {
   rmlMappingDialog.value?.show();
 }
 </script>
+<style scoped>
+.panel-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
+
+.panel-content {
+  flex: 1;
+  min-height: 0;
+  overflow: hidden;
+}
+</style>
