0e3f0a72868d16c6b3eb76f387abf18b233576bf
Devesh0129 <Devesh0129@users.noreply.github.com>
2026-04-15 00:15:29 +0000

    apply formatting changes

--- meta_configurator/src/components/panels/schema-diagram/VueFlowPanel.vue
+++ meta_configurator/src/components/panels/schema-diagram/VueFlowPanel.vue
@@ -152,7 +152,7 @@ async function pasteFromClipboard() {
     // Read JSON string from system clipboard
     const clipboardText = await navigator.clipboard.readText();
     const data = JSON.parse(clipboardText);
-    
+
     if (data.type === 'object') {
       const objectPath = addSchemaObject(schemaData, false, data);
       selectElement(objectPath);
