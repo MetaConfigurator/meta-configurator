db8a79d47979f28046fcc58440652a55801428f8
Devesh0129 <talsaniadevesh29@gmail.com>
2026-05-20 19:26:17 +0200

    Dialog box open twice issue resolved

--- meta_configurator/src/components/panels/gui-editor/properties/ReferenceProperty.vue
+++ meta_configurator/src/components/panels/gui-editor/properties/ReferenceProperty.vue
@@ -50,21 +50,20 @@ function onInput(event: Event) {
   const val = (event.target as HTMLInputElement).value;
   currentValue.value = val;
   const query = val.toLowerCase();
-  filteredSuggestions.value = query === ''
-    ? getDefinitions()
-    : getDefinitions().filter(p => p.toLowerCase().includes(query));
+  filteredSuggestions.value =
+    query === '' ? getDefinitions() : getDefinitions().filter(p => p.toLowerCase().includes(query));
 }
 
 function searchSuggestions(event: {query: string}) {
   const query = event.query.toLowerCase();
-  filteredSuggestions.value = query === ''
-    ? getDefinitions()
-    : getDefinitions().filter(p => p.toLowerCase().includes(query));
+  filteredSuggestions.value =
+    query === '' ? getDefinitions() : getDefinitions().filter(p => p.toLowerCase().includes(query));
 }
 
 function onItemSelect(event: {value: string}) {
   currentValue.value = event.value;
   emit('update:propertyData', event.value);
+  nextTick(() => autoCompleteRef.value?.hide());
 }
 
 function onBlur() {
