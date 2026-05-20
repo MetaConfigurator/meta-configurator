02ea3a906bdb4cb7743417c16ab67f9226748f6e
Felix Neubauer <33369081+Logende@users.noreply.github.com>
2026-05-20 14:15:11 +0200

    691 better way to deal with sub schema duplications in schema extraction (#978)
    * make schema extraction avoid duplicate subschemas
    
    * fix type errors

--- meta_configurator/src/data/managedSession.ts
+++ meta_configurator/src/data/managedSession.ts
@@ -6,7 +6,7 @@ import {getDataForMode, getSchemaForMode} from '@/data/useDataLink';
 import {SessionMode} from '@/store/sessionMode';
 import type {SearchResult} from '@/utility/search';
 import {EffectiveSchema} from '@/schema/effectiveSchemaCalculator';
-import type {ConfigDataTreeNode} from '@/components/panels/gui-editor/configDataTreeNode';
+import type {GuiEditorTreeNode} from '@/components/panels/gui-editor/configDataTreeNode';
 
 export class ManagedSession {
   constructor(public mode: SessionMode) {}
@@ -81,7 +81,7 @@ export class ManagedSession {
   /**
    * Returns true if the node is selected, or if the node or any of its children matches a search result.
    */
-  public isNodeHighlighted(node: ConfigDataTreeNode) {
+  public isNodeHighlighted(node: GuiEditorTreeNode) {
     const selectedPath = pathToString(this.currentSelectedElement.value);
     if (node.key && node.key === selectedPath) {
       return true;
